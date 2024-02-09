3e81cc60499d18fadcc329a5ff6b4b05ebac21fe
Dan Anstis <dan@bsod.co.nz>
2024-02-09 18:48:37 +1000

    Refactor main.go to use environment variable directly for port configuration.

--- cmd/asana-agent/main.go
+++ cmd/asana-agent/main.go
@@ -4,18 +4,9 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"strings"
 
 	"github.com/gorilla/mux"
-	"github.com/spf13/viper"
 )
-
-type Config struct {
-	Asana struct {
-		PAT string
-	}
-	Port string
-}
 
 func setupRouter() *mux.Router {
 	r := mux.NewRouter()
@@ -24,31 +15,15 @@ func setupRouter() *mux.Router {
 	// workspace routes
 	r.HandleFunc("/api/workspace", workspaceGetHandler).Methods("GET")
 	return r
-type Config struct {
-	Asana struct {
-		PAT string `mapstructure:"ASANA_PAT"`
-	}
-	Port string `mapstructure:"PORT"`
 }
 
 func main() {
-	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
-	viper.AutomaticEnv()
-
-	var config Config
-	viper.SetDefault("PORT", "8080") // Set a default port if none is specified
-	viper.BindEnv("ASANA.PAT")      // Bind the ASANA_PAT environment variable
-	viper.BindEnv("PORT")           // Bind the PORT environment variable
-	if err := viper.Unmarshal(&config); err != nil {
-		fmt.Printf("unable to decode into struct, %v", err)
-	}
-
 	r := setupRouter()
 	if config.Port == "" {
 		log.Fatal("Port must be set")
 	}
-	fmt.Printf("Web server starting at http://localhost:%s\n", config.Port)
-	err := http.ListenAndServe(":"+config.Port, r)
+	fmt.Printf("Web server starting at http://localhost:%s\n", port)
+	err := http.ListenAndServe(":"+port, r)
 	if err != nil {
 		log.Fatal(err)
 	}
