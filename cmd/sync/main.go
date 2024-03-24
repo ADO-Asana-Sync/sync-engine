17ec81ae717eb41fff2c56093fc1b1fd0ba75ea4
Dan Anstis <dan@bsod.co.nz>
2024-03-24 14:14:05 +1000

    Add context package and import necessary dependencies

--- cmd/sync/main.go
+++ cmd/sync/main.go
@@ -1,12 +1,18 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"log"
 	"os"
 
+	"github.com/ADO-Asana-Sync/sync-engine/internal/asana"
+	"github.com/ADO-Asana-Sync/sync-engine/internal/azure"
 	"github.com/ADO-Asana-Sync/sync-engine/internal/db"
 	"github.com/davecgh/go-spew/spew"
+	"github.com/golang/glog"
+	"github.com/uptrace/uptrace-go/uptrace"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/trace"
 )
 
 var (
@@ -16,38 +22,78 @@ var (
 )
 
 type App struct {
-	DB *db.DB
+	Asana           *asana.Asana
+	Azure           *azure.Azure
+	DB              *db.DB
+	Tracer          trace.Tracer
+	UptraceShutdown func(ctx context.Context) error
 }
 
 func main() {
-	log.Printf("Sync process started. Version: %v, commit: %v, date: %v", version, commit, date)
+	glog.Infof("Sync process started. Version: %v, commit: %v, date: %v", version, commit, date)
 	app := &App{}
 	err := app.setup()
 	if err != nil {
-		log.Fatalf("error setting up the app: %v", err)
+		glog.Fatalf("error setting up the app: %v", err)
 	}
 	defer func() {
-		err := app.DB.Client.Disconnect(nil)
+		err := app.DB.Client.Disconnect(context.Background())
 		if err != nil {
-			log.Fatalf("error disconnecting from the DB: %v", err)
+			glog.Fatalf("error disconnecting from the DB: %v", err)
+		}
+		err = app.UptraceShutdown(context.Background())
+		if err != nil {
+			glog.Fatalf("error shutting down Uptrace: %v", err)
 		}
 	}()
 
 	// List all projects
 	projects, err := app.DB.Projects()
 	if err != nil {
-		log.Fatalf("error listing projects: %v", err)
+		glog.Fatalf("error listing projects: %v", err)
 	}
 	spew.Dump(projects)
 }
 
 func (app *App) setup() error {
-	log.Printf("connecting to the DB")
+	// Uptrace setup.
+	glog.Infof("Connecting to Uptrace")
+	dsn := os.Getenv("UPTRACE_DSN")
+	environment := os.Getenv("UPTRACE_ENVIRONMENT")
+	uptrace.ConfigureOpentelemetry(
+		uptrace.WithServiceName("sync-engine"),
+		uptrace.WithDSN(dsn),
+		uptrace.WithServiceVersion(version),
+		uptrace.WithDeploymentEnvironment(environment),
+	)
+	app.UptraceShutdown = uptrace.Shutdown
+	app.Tracer = otel.Tracer("")
+
+	ctx := context.Background()
+	ctx, span := app.Tracer.Start(ctx, "sync.setup")
+	defer span.End()
+
+	// Database setup.
+	glog.Infof("connecting to the DB")
 	app.DB = &db.DB{}
 	connstr := os.Getenv("MONGO_URI")
-	err := app.DB.Connect(connstr)
+	err := app.DB.Connect(ctx, connstr)
 	if err != nil {
+		span.RecordError(err, trace.WithStackTrace(true))
 		return fmt.Errorf("error connecting to the DB: %v", err)
 	}
+
+	// Azure DevOps setup.
+	glog.Infof("connecting to Azure DevOps")
+	app.Azure = &azure.Azure{}
+	org := os.Getenv("ADO_ORG_URL")
+	pat := os.Getenv("ADO_PAT")
+	app.Azure.Connect(org, pat)
+
+	// Asana setup.
+	glog.Infof("connecting to Asana")
+	app.Asana = &asana.Asana{}
+	app.Asana.Connect(os.Getenv("ASANA_PAT"))
+
 	return nil
 }
