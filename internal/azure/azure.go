a19e879eec50688bc6ffcd2cf3ebaefcab07cc5a
Dan Anstis <dan@bsod.co.nz>
2024-03-24 14:13:49 +1000

    Add Azure connection and GetChangedWorkItems method

--- internal/azure/azure.go
+++ internal/azure/azure.go
@@ -1,1 +1,18 @@
 package azure
+
+import (
+	"github.com/microsoft/azure-devops-go-api/azuredevops/v7"
+)
+
+type Azure struct {
+	Client *azuredevops.Connection
+}
+
+func (a *Azure) Connect(orgUrl, pat string) {
+	clt := azuredevops.NewPatConnection(orgUrl, pat)
+	a.Client = clt
+}
+
+func (a *Azure) GetChangedWorkItems() {
+	// Get the changed work items.
+}
