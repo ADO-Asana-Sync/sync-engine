3286f7797094d03c8022a703f11e04a26c4d5df1
Dan Anstis <dan@bsod.co.nz>
2024-07-20 20:48:21 +1000

    chore: Update web UI routes and templates

--- cmd/web-ui/main.go
+++ cmd/web-ui/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"html/template"
 	"log"
 	"net/http"
 	"os"
@@ -13,7 +14,9 @@ func main() {
 		port = "8080"
 	}
 
-	http.HandleFunc("/", handler)
+	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/favicon.ico", faviconHandler)
+	http.HandleFunc("/projects", projectsHandler)
 
 	// Handler for static content
 	fs := http.FileServer(http.Dir(filepath.Join(".", "static")))
@@ -22,6 +25,31 @@ func main() {
 	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, filepath.Join("pages", "index.html"))
+func renderTemplate(w http.ResponseWriter, tmpl string, data interface{}) {
+	templates := template.Must(template.ParseFiles(
+		filepath.Join("templates", "main.html"),
+		filepath.Join("templates", tmpl+".html"),
+	))
+
+	err := templates.ExecuteTemplate(w, "main", data)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func faviconHandler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, filepath.Join("static", "favicon.ico"))
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "index", map[string]interface{}{
+		"Title":       "Home",
+		"CurrentPage": "home",
+	})
+}
+func projectsHandler(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "projects", map[string]interface{}{
+		"Title":       "Projects",
+		"CurrentPage": "projects",
+	})
 }
