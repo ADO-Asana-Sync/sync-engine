c227c4d4913d7249082ed68958d25571ab243419
Dan Anstis <dan@bsod.co.nz>
2024-07-21 12:23:43 +1000

    chore: refactor handlers into own files

---
+++ cmd/web-ui/routes.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"net/http"
+	"path/filepath"
+)
+
+func registerRoutes(mux *http.ServeMux, app *App) {
+	// Handlers for shared content.
+	fs := http.FileServer(http.Dir(filepath.Join(".", "static")))
+	mux.Handle("/static/", http.StripPrefix("/static/", fs))
+	mux.HandleFunc("/favicon.ico", func(w http.ResponseWriter, r *http.Request) {
+		faviconHandler(w, r)
+	})
+
+	// Dashboard routes.
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		homeHandler(w, r)
+	})
+
+	// Projects routes.
+	mux.HandleFunc("/projects", func(w http.ResponseWriter, r *http.Request) {
+		projectsHandler(app, w, r)
+	})
+	mux.HandleFunc("/add-project", func(w http.ResponseWriter, r *http.Request) {
+		addProjectHandler(app, w, r)
+	})
+}
