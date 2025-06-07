b6a4c53aa3e163dd508352f8b72a3588e8472548
Dan Anstis <dan@bsod.co.nz>
2025-06-07 17:45:38 +1000

    refactor(error handling): use constant for error formatting in task retrieval

--- internal/db/task.go
+++ internal/db/task.go
@@ -11,6 +11,9 @@ import (
 	"golang.org/x/net/context"
 )
 
+// ErrorFmtFindingTask is the error format for when a task cannot be found
+const ErrorFmtFindingTask = "error finding task: %v"
+
 // TasksCollection is the name of the collection in the database for synced tasks.
 var TasksCollection = "tasks"
 
@@ -80,7 +83,7 @@ func (db *DB) TaskByID(ctx context.Context, id primitive.ObjectID) (TaskMapping,
 	collection := db.Client.Database(DatabaseName).Collection(TasksCollection)
 	err := collection.FindOne(ctx, bson.M{"_id": id}).Decode(&task)
 	if err != nil {
-		err = fmt.Errorf("error finding task: %v", err)
+		err = fmt.Errorf(ErrorFmtFindingTask, err)
 		span.RecordError(err)
 		return task, err
 	}
@@ -116,7 +119,7 @@ func (db *DB) TaskByIDs(ctx context.Context, adoProjectID string, adoTaskID int,
 
 	err := collection.FindOne(ctx, filter).Decode(&task)
 	if err != nil {
-		err = fmt.Errorf("error finding task: %v", err)
+		err = fmt.Errorf(ErrorFmtFindingTask, err)
 		span.RecordError(err)
 		return task, err
 	}
@@ -136,7 +139,7 @@ func (db *DB) TaskByADOTaskID(ctx context.Context, id int) (TaskMapping, error) 
 	collection := db.Client.Database(DatabaseName).Collection(TasksCollection)
 	err := collection.FindOne(ctx, bson.M{"ado_task_id": id}).Decode(&task)
 	if err != nil {
-		err = fmt.Errorf("error finding task: %v", err)
+		err = fmt.Errorf(ErrorFmtFindingTask, err)
 		span.RecordError(err)
 		return task, err
 	}
